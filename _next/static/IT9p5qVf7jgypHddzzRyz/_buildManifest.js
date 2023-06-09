1753e8ed083cb081618c372181d718478374cc03
Sesame-minmin <Sesame-minmin@users.noreply.github.com>
2023-06-09 02:38:53 +0000

    deploy: 2d020b0549dbbc445c76d4d2d1bd368c85b75ffd

--- _next/static/IT9p5qVf7jgypHddzzRyz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b4ee623f9d11ddf3.css","static/chunks/pages/index-e3285c7685d322de.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/QJeN9V7HCrq2BTLx2u3Pl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a7582622ad95c8af.css","static/chunks/pages/index-f2b1970f0d5056ea.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
