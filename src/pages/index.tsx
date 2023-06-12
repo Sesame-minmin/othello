26f7736971905acde8d31d577f1ebdfb0715fb49
Sesame-minmin <135975020+Sesame-minmin@users.noreply.github.com>
2023-06-12 13:29:31 +0900

    なんか直ったらしい

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,26 +29,41 @@ const Home = () => {
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
     const gameJudge: number[] = JSON.parse(JSON.stringify(judge));
     if (board[y][x] === 3) {
+      ++gameJudge[0];
       for (const value of directions) {
+        let j = 0;
         for (let i = 1; i < 8; i++) {
           if (
             board[y + value[0] * i] !== undefined &&
-            board[y + value[0] * i][x + value[1] * i] === turnColor &&
-            board[y + value[0]][x + value[1]] === 3 - turnColor
+            board[y + value[0] * i][x + value[1] * i] === 3 - turnColor
           ) {
-            for (let j = i - 1; j > 0; j--) {
+            ++j;
+          } else if (
+            board[y + value[0] * i] !== undefined &&
+            board[y + value[0] * i][x + value[1] * i] % 3 === 0
+          ) {
+            break;
+          } else if (
+            board[y + value[0] * i] !== undefined &&
+            board[y + value[0] * i][x + value[1] * i] === turnColor
+          ) {
+            if (board[y + value[0] * i][x + value[1] * i] % 3 === 0) {
+              break;
+            }
+            for (j - 1; j > 0; j--) {
               newBoard[y + value[0] * j][x + value[1] * j] = turnColor;
             }
-          }
-        }
-        for (let i = 0; i < 8; i++) {
-          for (let j = 0; j < 8; j++) {
-            newBoard[i][j] = newBoard[i][j] % 3;
           }
         }
       }
       newBoard[y][x] = turnColor;
       setTurnColor(3 - turnColor);
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          newBoard[i][j] = newBoard[i][j] % 3;
+        }
+      }
+
       let J1 = 0;
       for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
@@ -61,10 +76,11 @@ const Home = () => {
               newBoard[i + value[0]][j + value[1]] === turnColor &&
               newBoard[i][j] === 0
             ) {
-              for (let k = 1; k < 8; k++) {
+              for (let k = 1; k < 7; k++) {
                 if (
-                  newBoard[i + value[0] * k] !== undefined &&
-                  newBoard[i + value[0] * k][j + value[1] * k] === 3 - turnColor
+                  newBoard[i + value[0] * (k + 1)] !== undefined &&
+                  newBoard[i + value[0] * k][j + value[1] * k] === turnColor &&
+                  newBoard[i + value[0] * (k + 1)][j + value[1] * (k + 1)] === 3 - turnColor
                 ) {
                   newBoard[i][j] = 3;
                 }
@@ -74,15 +90,15 @@ const Home = () => {
         }
       }
       gameJudge[1] = J1;
-      ++gameJudge[0];
-      //なんで総石数でずれが生じているんだ？
-      //しかも本来の数より少ないんだよな
-      //なんかうまくいったわ草
-      gameJudge[2] = gameJudge[0] - gameJudge[1];
-      console.log(gameJudge[0]);
-      setBoard(newBoard);
-      setJudge(gameJudge);
     }
+
+    //なんで総石数でずれが生じているんだ？
+    //しかも本来の数より少ないんだよな
+    //なんかうまくいったわ草
+    gameJudge[2] = gameJudge[0] - gameJudge[1];
+    console.log(gameJudge[0], turnColor);
+    setBoard(newBoard);
+    setJudge(gameJudge);
   };
 
   return (
