c8b04875a30c26e96924a65ab560ebfa134339d2
Sesame-minmin <Sesame-minmin@users.noreply.github.com>
2023-07-22 03:50:32 +0000

    deploy: 9b7f289d3d18235b85e14f4561b2abf59a5159dd

--- _next/static/sHqupAUryy6nZqaUbx8Y9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b76de8a0bb513484.css","static/chunks/pages/index-f4cb5f43804f05f7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
